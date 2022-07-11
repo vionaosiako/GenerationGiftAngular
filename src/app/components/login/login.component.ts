8a5466054a829a467cc60542b6568039e4bad8da
vionaosiako <viona.osiako@student.morigaschool.com>
2022-07-11 16:28:03 +0300

    ft log in

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  login: any;
+  router:any;
+  constructor(public _userService: UserService,public_router: Router) { }
+  ngOnInit() {
+    this.login = {
+      username: '',
+      password: ''
+    };
   }
-
+  home() {
+    this.router.navigate(['/','']);
+  }
+  loginUser() {
+    this._userService.loginUsers(this.login).subscribe(
+      response => {
+        alert('Login successful!')
+      },
+      error =>{
+        alert('Error!')
+      }
+    );
+  }
+  refreshToken() {
+    this._userService.refreshToken();
+  }
+  logout() {
+    this._userService.logout();
+  }
 }
